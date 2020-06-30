10058c802427fd7c08ebaaaca230216f00feec56
limzykenneth <limzy.kenneth@gmail.com>
2020-06-30 14:48:52 +0100

    Header's structure

--- frontend/components/App.vue
+++ frontend/components/App.vue
@@ -1,12 +1,16 @@
 <template>
-
+	<main id="page-container">
+		<app-header></app-header>
+	</main>
 </template>
 
 <script>
+import Header from "./Header.vue";
+
 export default {
 	name: "App",
 	components: {
-
+		"app-header": Header
 	},
 	methods: {
 
@@ -15,5 +19,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+#page-container{
+	background-color: #242424;
+	min-width:100vw;
+	min-height: 100vh;
+}
 </style>
---
+++ frontend/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<header id="page-header">
+		<h1>IED 2020</h1>
+
+		<div>
+			<p>Information Experience Design</p>
+			<p>Royal College of Art</p>
+			<p>London</p>
+		</div>
+
+		<div>
+			<ul>
+				<li>about</li>
+				<li>instagram</li>
+				<li>RCA2020</li>
+			</ul>
+		</div>
+	</header>
+</template>
+
+<script>
+export default {
+	name: "Header"
+};
+</script>
+
+<style>
+
+</style>
