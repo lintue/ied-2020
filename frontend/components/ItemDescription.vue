51d0fed8486dd928f7e89a34e6231480c218f243
limzykenneth <limzy.kenneth@gmail.com>
2020-07-09 15:15:39 +0100

    Description box finish

--- frontend/components/ItemDescription.vue
+++ frontend/components/ItemDescription.vue
@@ -2,6 +2,35 @@
 	<div id="item-content"
 			v-on:click.stop=""
 	>
+		<section id="title-bar">
+			<div id="file-name">{{ entry.workTitle || "Description" }}</div>
+			<div id="close-item-btn"
+				v-on:click="closeItem"
+			>
+				<span class="material-icons">
+					close
+				</span>
+			</div>
+		</section>
+
+		<section id="name"
+			v-if="entry.student"
+		>
+			{{ entry.student }}
+		</section>
+
+		<section id="description"
+			v-if="entry.description"
+		>
+			<h2 id="project-title">
+				{{ entry.workTitle || "Description" }}
+			</h2>
+			<p id="description-text"
+				v-if="entry.description"
+			>
+				{{ entry.description }}
+			</p>
+		</section>
 	</div>
 </template>
 
@@ -27,5 +56,62 @@ export default{
 
 #item-content{
 	.dark-theme();
+
+	max-width: 1000px;
+	width: 70%;
+	border: 1px solid;
+	position: absolute;
+	left: 50%;
+	transform: translateX(-50%);
+	padding-bottom: 2rem;
+
+	#title-bar{
+		text-align: center;
+		font-size: 1.7rem;
+		border-bottom: 1px solid;
+
+		#file-name{
+			padding: 0.5rem;
+			display: inline-block;
+			font-weight: 500;
+		}
+
+		#close-item-btn{
+			float: right;
+			display: inline-block;
+			padding: 0.4rem;
+			font-size: 1.9rem;
+			cursor: pointer;
+			border-left: 1px solid;
+		}
+	}
+
+	#name{
+		.light-theme();
+
+		font-family: "Montserrat", sans-serif;
+		font-weight: 800;
+		font-size: 1.5rem;
+		font-style: italic;
+		padding: 0.5rem 3rem;
+		margin: 2rem 3rem;
+		border: 3px solid;
+		display: inline-block;
+	}
+
+	#description{
+		padding: 0 3rem;
+
+		#project-title{
+			font-family: "Montserrat", sans-serif;
+			font-weight: 700;
+			font-size: 2.2rem;
+			margin: 0;
+		}
+
+		#description-text{
+			font-weight: 400;
+		}
+	}
 }
 </style>
